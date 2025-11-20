f10e9a7210be65693bc03e5f7f022a6519f2483e
Daniella Duncan <dago0001@stud.ek.dk>
2025-11-20 13:51:35 +0100

    Dark mode

---
+++ js/darkmode.js
@@ -0,0 +1,9 @@
+// Tilføj din JavaScrconst html =
+const html = document.querySelector("html");
+const btn = document.querySelector("button");
+
+function toggleTheme() {
+  html.classList.toggle("dark");
+}
+
+btn.addEventListener("click", toggleTheme);
--- js/main.js
+++ js/main.js
@@ -50,21 +50,22 @@ function resetInfo() {
 function updateInfoboxOre() {
   title.textContent = "Dårligt hørende – Manglende reaktion på alarm";
   placeholder.innerHTML = "Ældre med hørenedsættelse reagerer ofte ikke på brandalarmer, råb eller evakueringsbeskeder. Det øger risikoen for, at nogle ikke kommer ud i tide.";
-  efficiency.innerHTML = "Fakta / Statistik: <li>70% af personer over 70 år har en form for høretab.</li><li> I flere brande på plejehjem er beboere ikke blevet vækket af sirener.</li>";
-  requirement.innerHTML = "Mulige løsninger: <li> Kombinér lydalarmer med blinkende lys eller vibrationspuder.</li>  <li>Brug tydelige håndbevægelser og visuelle instruktioner.</li>";
+  efficiency.innerHTML = "<p><strong>Fakta / Statistik:</strong> <li>70% af personer over 70 år har en form for høretab.</li><li> I flere brande på plejehjem er beboere ikke blevet vækket af sirener.</li></p>";
+  requirement.innerHTML = "<p><strong>Mulige løsninger:</strong> <li> Kombinér lydalarmer med blinkende lys eller vibrationspuder.</li>  <li>Brug tydelige håndbevægelser og visuelle instruktioner.</li></p>";
 }
 
 function updateInfoboxOje() {
   title.textContent = "Dårligt seende / desorienteret – Risiko for fald og panik";
   placeholder.innerHTML = "<p>Nedsat syn, forvirring eller balanceproblemer gør, at ældre kan blive desorienterede under evakuering. Nogle mister retningen, snubler eller falder i panik.</p>";
-  efficiency.innerHTML = "<p>Fakta / Statistik: <li>Hver tredje over 75 år har problemer med synet, selv med briller.</li><li> 40% af faldulykker på plejehjem sker under stressede situationer.</li> </p>";
+  efficiency.innerHTML = "<p><strong>Fakta / Statistik:</strong> <li>Hver tredje over 75 år har problemer med synet, selv med briller.</li><li> 40% af faldulykker på plejehjem sker under stressede situationer.</li> </p>";
   requirement.innerHTML =
-    "Mulige løsninger: <li> Brug lysende gulvmarkeringer og tydelig belysning mod udgange.</li> <li> Fjern løse tæpper og forhindringer i flugtveje.</li>  <li>Sørg for, at personalet ledsager de mest udsatte beboere fysisk.</li>";
+    "<p><strong>Mulige løsninger: </strong><li> Brug lysende gulvmarkeringer og tydelig belysning mod udgange.</li> <li> Fjern løse tæpper og forhindringer i flugtveje.</li>  <li>Sørg for, at personalet ledsager de mest udsatte beboere fysisk.</li></p>";
 }
 
 function updateInfoboxBen() {
   title.textContent = "Dårligt gående – Langsom evakuering";
   placeholder.innerHTML = "<p>En stor del af ældre beboere har nedsat mobilitet. Det betyder, at de bevæger sig langsomt, har brug for hjælpemidler eller helt afhænger af personale for at komme ud.</p>";
-  efficiency.innerHTML = "<p>Fakta / Statistik:<li>6 ud af 10 plejehjemsbeboere har brug for hjælp til at bevæge sig fra seng til stol</li><li>Evakuering kan tage op til 3 gange længere tid end hos raske voksne.</li></p>";
-  requirement.innerHTML = "Mulige løsninger:<li> Brug særlige evakueringsstole ved trapper.</li>  <li>Tilknyt personale til bestemte beboere ved evakuering.</li> <li>Planlæg realistiske evakueringsøvelser med fokus på langsom mobilitet.</li>";
+  efficiency.innerHTML = "<p><strong>Fakta / Statistik:</strong><li>6 ud af 10 plejehjemsbeboere har brug for hjælp til at bevæge sig fra seng til stol</li><li>Evakuering kan tage op til 3 gange længere tid end hos raske voksne.</li></p>";
+  requirement.innerHTML =
+    "<p><strong>Mulige løsninger:</strong><li> Brug særlige evakueringsstole ved trapper.</li>  <li>Tilknyt personale til bestemte beboere ved evakuering.</li> <li>Planlæg realistiske evakueringsøvelser med fokus på langsom mobilitet.</li></p>";
 }
